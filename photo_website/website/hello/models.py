34710c768a4668f89a976916f75ea354db1d68eb
Seth <eug119@my.utsa.edu>
2019-02-04 20:18:34 -0600

    Serializers and Models from hello app needed to be added.  Image upload end point relies on this model.

---
+++ photo_website/website/hello/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Songs(models.Model):
+    # song title
+    title = models.CharField(max_length=255, null=False)
+    # name of artist or group/band
+    artist = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return "{} - {}".format(self.title, self.artist)
+
+
+class ImageModel(models.Model):
+    model_pic = models.ImageField('Upload Image')
